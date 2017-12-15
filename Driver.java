baf597d57a82cfa60b2327f4808c9fd6683132dc
alexdonahoe95 <34562687+alexdonahoe95@users.noreply.github.com>
2017-12-15 15:19:56 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -85,6 +85,73 @@ public class Driver {
         }
        
     }
+	  public static void serializeCus(LinkedPositionalList<CustomerType> e)
+     {
+ 		Position<CustomerType> cursor = e.first();
+ 		ArrayList<CustomerType> cus = new ArrayList<CustomerType>();
+         
+ 		while (cursor != null)
+        {
+ 			cus.add(cursor.getElement( ));
+         	cursor = e.after(cursor);
+         
+        }       
+            try
+             {   
+                 //Saving of object in a file
+                 FileOutputStream file = new FileOutputStream("customers.txt");
+                 ObjectOutputStream out = new ObjectOutputStream(file);
+                  
+                 // Method for serialization of object
+                 out.writeObject(cus);
+                  
+                 out.close();
+                 file.close();
+                  
+                 System.out.println("Object has been serialized");
+     
+             }
+              
+             catch(IOException ex)
+             {
+                 System.out.println("IOException is caught");
+             }
+     
+     }
+     public static void deserializeCus()
+     {    
+   	
+         
+         try
+         {   
+             // Reading the object from a file
+             FileInputStream file = new FileInputStream("customers.txt");
+             ObjectInputStream in = new ObjectInputStream(file);
+              
+             // Method for deserialization of object
+             ArrayList<CustomerType> cus = (ArrayList<CustomerType>)in.readObject();
+             for(int i = 0; i < cus.size(); i++)
+             {
+             	cuslist.addFirst(cus.get(i));
+             }
+             in.close();
+             file.close();
+              
+             System.out.println("Object has been deserialized ");
+
+         }
+          
+         catch(IOException ex)
+         {
+             System.out.println("IOException is caught");
+         }
+          
+         catch(ClassNotFoundException ex)
+         {
+             System.out.println("ClassNotFoundException is caught");
+         }
+     
+ }
 
 
 }
