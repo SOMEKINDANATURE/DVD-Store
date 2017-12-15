fceaf4e3c0ab7ed3a5ae135e2d14bb2523d959ac
alexdonahoe95 <34562687+alexdonahoe95@users.noreply.github.com>
2017-12-15 16:32:33 -0600

    Add files via upload

---
+++ CheckedOut.java
@@ -0,0 +1,43 @@
+
+public class CheckedOut 
+{
+	CustomerType customer;
+	DVD[] dvd = new DVD[5];
+	int cnt = 0;
+	public void CheckedOut(CustomerType cus, DVD d)
+	{
+		customer = cus;
+		if (cnt < 5)
+		{
+			dvd[cnt] = d;
+			cnt++;
+		}
+		else
+		{
+			System.out.println("You cant have more than 5 movies checked out at a single time");
+			
+		}
+	}
+	public void findMovie(String title)
+	{
+		for(int i = cnt; i > 0; i-- )
+		{
+			if(dvd[i].Title.equals(title))
+			{
+				dvd[i].Return();
+				dvd[i] = null;
+			}
+		}
+	}
+	public int getCnt()
+	{
+		return cnt;
+	}
+	
+	public int getId()
+	{
+		return customer.acct_number;
+	}
+	
+
+}
