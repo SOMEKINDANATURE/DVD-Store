58b51faf7751679740cd886a0cee7e5e1384be19
alexdonahoe95 <34562687+alexdonahoe95@users.noreply.github.com>
2017-12-15 15:24:15 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -150,8 +150,45 @@ public class Driver {
          {
              System.out.println("ClassNotFoundException is caught");
          }
-     
+	  
  }
+		public static void addDvd()
+	{
+		DVD pulpFiction = new DVD("Pulp Fiction",2,"Harvey Weinstein"," Quentin Tarantino", "Miramax", 5);
+		pulpFiction.setStar("Bruce Willis");
+		pulpFiction.setStar("Uma Thruman");
+	
+		DVD starWars = new DVD("Star Wars",2,"Gary Kurtz","George Lucas", "20th Century Fox", 3);
+		starWars.setStar("Mark Hammil");
+		starWars.setStar("Harrison Ford");	
+		DVD clerks = new DVD("Clerks",2,"Scott Mosier","Kevin Smith", "Askew", 2);
+		clerks.setStar("Brian O'Halloran");
+		clerks.setStar("Jeff Anderson");
+		DVD paulBlart = new DVD("Paul Blart",2,"Barry Bernadi","Steve Carr", "Columbia", 4);
+		paulBlart.setStar("Kevin James");
+		paulBlart.setStar("Keit O'Donnell");
+
+		DVD piratesCarribean = new DVD();
+		DVDList.addFirst(pulpFiction);
+		DVDList.addLast(starWars);
+		DVDList.addLast(clerks);
+		DVDList.addLast(paulBlart);
+		DVDList.addLast(piratesCarribean);
+		
+	}
+	public static void addCus()
+	{
+		CustomerType jack = new CustomerType("Jack","8778 wallaby way, Sydney", "Jack.Rowell95@gmail.com", 1998);
+		CustomerType Curt = new CustomerType("Curt","2032 Joel dr, Muskego", "Curt.Cherti95@gmail.com", 5085);
+		CustomerType Jason = new CustomerType("Jason","1111 Fake st, Hollywood", "Jason.Poole95@gmail.com", 2018);
+		CustomerType Nate = new CustomerType("Nate","1989 old st, forgotten", "Nate.Rolbiecki@gmail.com", 1372);
+		CustomerType Kyle = new CustomerType("Kyle","2020 vision way, vision", "Kyle.Endl@gmail.com",3675);
+		cuslist.addFirst(jack);
+		cuslist.addLast(Curt);
+		cuslist.addLast(Jason);
+		cuslist.addLast(Nate);
+		cuslist.addLast(Kyle);
+	}
 
 
 }
